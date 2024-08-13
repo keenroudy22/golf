df3468ce1e250f3f8f7fd14b07f5c0574ca4ec2e
keenroudy22 <kroudebush96@gmail.com>
2024-08-13 13:14:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def index():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Golf Club Recommender</title>
+        <title>Virtual Caddie</title>
         <style>
             body {
                 font-family: 'Arial', sans-serif;
